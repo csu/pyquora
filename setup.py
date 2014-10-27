a6c61d357b802bfdee3cffa0ff64d43f82c74a6c
Christopher Su <christophersu9@gmail.com>
2014-10-27 15:28:24 -0700

    pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='quora',
-    version='0.1.7',
+    version='0.1.8',
     description='Fetches and parses data from Quora.',
     author='Christopher Su',
     author_email='christophersu9@gmail.com',
