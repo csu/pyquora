a5712cfe36a93634dee6c25ceb964ead235ff5a5
Christopher Su <christophersu9@gmail.com>
2014-12-22 19:03:26 +0100

    tests

--- tests/test_activity.py
+++ tests/test_activity.py
@@ -5,23 +5,28 @@ class TestActivity:
     q = Quora()
     activity1 = q.get_activity('Christopher-J-Su')
     activity2 = q.get_activity('Aaron-Ounn')
+    test_activities = [activity1, activity2]
 
     @classmethod
     def setup_class(cls):
         print "Setup here"
 
     def test_activity_answers(self):
-        assert self.activity1.answers
-        assert self.activity2.answers
+        for activity in self.test_activities:
+            assert activity.answers
+            assert activity.answers
 
     def test_activity_want_answers(self):
-        assert self.activity1.want_answers
-        assert self.activity2.want_answers
+        for activity in self.test_activities:
+            assert activity.want_answers
+            assert activity.want_answers
 
     def test_activity_user_follows(self):
-        assert self.activity1.user_follows
-        assert self.activity2.user_follows
+        for activity in self.test_activities:
+            assert activity.user_follows
+            assert activity.user_follows
 
     def test_activity_upvotes(self):
-        assert self.activity1.upvotes
-        assert self.activity2.upvotes
+        for activity in self.test_activities:
+            assert activity.user_follows
+            assert activity.user_follows
