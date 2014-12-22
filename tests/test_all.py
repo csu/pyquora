a4ad46b62eb863f01a05eb1767101ae928511331
Christopher Su <christophersu9@gmail.com>
2014-12-22 08:53:42 +0100

    we need better tests

--- tests/test_all.py
+++ tests/test_all.py
@@ -5,7 +5,6 @@ class TestActivity:
     quoraObj = Quora()
     activity1 = quoraObj.get_activity('Christopher-J-Su')
     activity2 = quoraObj.get_activity('Aaron-Ounn')
-    activity3 = quoraObj.get_activity('Elynn-Lee')
 
     @classmethod
     def setup_class(cls):
@@ -14,19 +13,15 @@ class TestActivity:
     def test_activity_answers(self):
         assert self.activity1.answers
         assert self.activity2.answers
-        assert self.activity3.answers
 
     def test_activity_want_answers(self):
         assert self.activity1.want_answers
         assert self.activity2.want_answers
-        assert self.activity3.want_answers
 
     def test_activity_user_follows(self):
         assert self.activity1.user_follows
         assert self.activity2.user_follows
-        assert self.activity3.user_follows
 
     def test_activity_upvotes(self):
         assert self.activity1.upvotes
         assert self.activity2.upvotes
-        assert self.activity3.upvotes
