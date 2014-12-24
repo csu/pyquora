4361b716e2ac536a8b6e8329e1643ee048f63a4e
Christopher Su <christophersu9@gmail.com>
2014-12-24 09:32:10 +0100

    pypi update

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README') as file:
 
 setup(
     name='quora',
-    version='0.1.18',
+    version='0.1.18.1',
     description='Fetches and parses data from Quora.',
     long_description=long_description,
     author='Christopher Su',
