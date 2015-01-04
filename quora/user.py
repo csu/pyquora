74c251b18b1727bcd0f01ba5c12f7895a45b140f
Christopher Su <christophersu9@gmail.com>
2015-01-04 08:21:03 -0800

    make user parameter optional for legacy purposes

--- quora/user.py
+++ quora/user.py
@@ -16,6 +16,7 @@ def enum(*sequential, **named):
 
 ACTIVITY_ITEM_TYPES = enum(UPVOTE=1, USER_FOLLOW=2, WANT_ANSWER=3, ANSWER=4, REVIEW_REQUEST=5)
 LOG_ENTRY_TYPES     = enum(ANSWER_ADDED=1, ANSWER_DELETED=2, COMMENT=2, EDIT=3, TOPIC=4)
+DEFAULT_USER        = None
 
 ####################################################################
 # Helpers
@@ -132,7 +133,7 @@ def check_log_type(log_entry):
 # API
 ####################################################################
 class User:
-    def __init__(self, user):
+    def __init__(self, user=DEFAULT_USER):
         self.user = user
         self._stats = None
         self._activity = None
