62951af9d1299dde4fb07482d63f414a3fd4383f
rohithpr <praroh2@gmail.com>
2015-02-09 20:30:40 +0530

    Fix typo

--- quora/quora.py
+++ quora/quora.py
@@ -6,17 +6,18 @@ import requests
 # Helpers
 ####################################################################
 def try_cast_int(s):
-    pattern = re.compile(r'([0-9]+(\.[0-9]+)*[ ]*[Kk])|([0-9]+)')
-    raw_result = re.search(pattern, s).groups()
-    if raw_result[2] != None:
-        return int(raw_result[2])
-    elif raw_result[1] == None:
-        raw_result = re.search(r'([0-9]+)', raw_result[0])
-        return int(raw_result.groups()[0]) * 1000
-    else:
-        raw_result = re.search(r'([0-9]+)\.([0-9]+)', raw_result[0]).groups()
-        return int(raw_result[0]) * 1000 + int(raw_result[1]) * 100
-    except ValueError:
+    try:
+        pattern = re.compile(r'([0-9]+(\.[0-9]+)*[ ]*[Kk])|([0-9]+)')
+        raw_result = re.search(pattern, s).groups()
+        if raw_result[2] != None:
+            return int(raw_result[2])
+        elif raw_result[1] == None:
+            raw_result = re.search(r'([0-9]+)', raw_result[0])
+            return int(raw_result.groups()[0]) * 1000
+        else:
+            raw_result = re.search(r'([0-9]+)\.([0-9]+)', raw_result[0]).groups()
+            return int(raw_result[0]) * 1000 + int(raw_result[1]) * 100
+    except:
         return s
 
 def get_question_link(soup):
