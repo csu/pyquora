6df4cdd8fe629fabadd792d0b7507a57f9dc37b7
Christopher Su <christophersu9@gmail.com>
2015-02-09 11:21:42 -0800

    refactor tests

--- tests/test_helper_functions.py
+++ tests/test_helper_functions.py
@@ -1,11 +1,9 @@
 import quora
 
-def test_try_cast_int():
-	assert quora.try_cast_int('200 Upvotes') == 200
-	assert quora.try_cast_int('2k Upvotes') == 2000
-	assert quora.try_cast_int('2 K Upvotes') == 2000
-	assert quora.try_cast_int('2.3k Upvotes') == 2300
-	assert quora.try_cast_int('2.3 K Upvotes') == 2300
-	assert quora.try_cast_int('<span class="count">3</span>') == 3
+class TestHelperFunctions:
+    def test_try_cast_int(self):
+        input_strings = ['200 Upvotes', '2k Upvotes', '2 K Upvotes', '2.3k Upvotes', '2.3 K Upvotes', '<span class="count">3</span>']
+        expected = [200, 2000, 2000, 2300, 2300, 3]
 
-test_try_cast_int()
+        for i in range(0, len(input_strings)):
+            assert quora.try_cast_int(input_strings[i]) == expected[i]
