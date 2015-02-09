bd766630ceadcea1928a8b582a178d49788ecc53
rohithpr <praroh2@gmail.com>
2015-02-09 20:20:33 +0530

    Fix try_cast_int #66

--- quora/quora.py
+++ quora/quora.py
@@ -6,10 +6,16 @@ import requests
 # Helpers
 ####################################################################
 def try_cast_int(s):
-    try:
-        temp = re.findall('\d', str(s))
-        temp = ''.join(temp)
-        return int(temp)
+    pattern = re.compile(r'([0-9]+(\.[0-9]+)*[ ]*[Kk])|([0-9]+)')
+    raw_result = re.search(pattern, s).groups()
+    if raw_result[2] != None:
+        return int(raw_result[2])
+    elif raw_result[1] == None:
+        raw_result = re.search(r'([0-9]+)', raw_result[0])
+        return int(raw_result.groups()[0]) * 1000
+    else:
+        raw_result = re.search(r'([0-9]+)\.([0-9]+)', raw_result[0]).groups()
+        return int(raw_result[0]) * 1000 + int(raw_result[1]) * 100
     except ValueError:
         return s
 
