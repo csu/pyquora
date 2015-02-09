99bacc16848d513694e1ca98464767c7eb238664
Christopher Su <christophersu9@gmail.com>
2015-02-09 11:53:32 -0800

    rst readme for pypi

--- gen_rst_readme.py
+++ gen_rst_readme.py
@@ -1,10 +1,19 @@
 import pandoc
 import os
+import re
 
 pandoc.core.PANDOC_PATH = '/usr/local/bin/pandoc'
 
 doc = pandoc.Document()
 doc.markdown = open('README.md').read()
+
+filtered = str(doc.rst)
+filtered = re.sub('Table of Contents\n~~~~~~~~~~~~~~~~~.*Installation\n------------', 'Installation\n------------', filtered, flags=re.DOTALL)
+filtered = re.sub('\n`\|Build Status\| <https://travis-ci.org/csu/pyquora>`_ `\|Latest\nVersion\| <https://pypi.python.org/pypi/quora/>`_\n', '', filtered, flags=re.DOTALL)
+filtered = re.sub('`\|Gitter\|.*>`_', '', filtered, flags=re.DOTALL)
+filtered = re.sub('`\|HuBoard\|.*`_', '', filtered, flags=re.DOTALL)
+filtered = re.sub('Contribute\n----------.*', '', filtered, flags=re.DOTALL)
+
 f = open('README', 'w+')
-f.write(doc.rst)
+f.write(filtered)
 f.close()
