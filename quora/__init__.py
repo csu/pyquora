c646bdd7cefb39b74ebeb5b89c84273f32949bd4
rohithpr <praroh2@gmail.com>
2015-02-10 19:48:33 +0530

    Changed try_cast_int in user.py #66

--- quora/__init__.py
+++ quora/__init__.py
@@ -1,2 +1,2 @@
 from user import User, Activity
-from quora import Quora,try_cast_int
+from quora import Quora, try_cast_int
--- quora/user.py
+++ quora/user.py
@@ -23,10 +23,17 @@ DEFAULT_USER        = None
 ####################################################################
 def try_cast_int(s):
     try:
-        temp = re.findall('\d', str(s))
-        temp = ''.join(temp)
-        return int(temp)
-    except ValueError:
+        pattern = re.compile(r'([0-9]+(\.[0-9]+)*[ ]*[Kk])|([0-9]+)')
+        raw_result = re.search(pattern, s).groups()
+        if raw_result[2] != None:
+            return int(raw_result[2])
+        elif raw_result[1] == None:
+            raw_result = re.search(r'([0-9]+)', raw_result[0])
+            return int(raw_result.groups()[0]) * 1000
+        else:
+            raw_result = re.search(r'([0-9]+)\.([0-9]+)', raw_result[0]).groups()
+            return int(raw_result[0]) * 1000 + int(raw_result[1]) * 100
+    except:
         return s
 
 def get_name(source):
