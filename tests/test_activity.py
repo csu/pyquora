0f75b3dbbd8f9ce5ba5c8e1f222ce3cefc249d5e
Christopher Su <christophersu9@gmail.com>
2015-02-10 18:11:07 -0800

    fix tests

--- tests/test_activity.py
+++ tests/test_activity.py
@@ -5,20 +5,8 @@ class TestActivity:
     test_activities.append(User('Christopher-J-Su').activity)
     test_activities.append(User('Aaron-Ounn').activity)
 
-    def test_activity_answers(self):
+    def test_activity(self):
         for activity in self.test_activities:
-            assert activity.answers
-
-    def test_activity_want_answers(self):
-        for activity in self.test_activities:
-            assert activity.want_answers
-
-    def test_activity_user_follows(self):
-        for activity in self.test_activities:
-            assert activity.user_follows
-
-    def test_activity_upvotes(self):
-        for activity in self.test_activities:
-            assert activity.user_follows
+            assert activity['activity']
 
     # TODO: add tests to ensure that all fields in all types of activity are valid
