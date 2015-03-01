aa835c7ce684f279a3170ee8d60299c45e441a29
Christopher Su <christophersu9@gmail.com>
2015-02-28 18:34:07 -0800

    fix legacy api

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README') as file:
 
 setup(
     name='quora',
-    version='0.1.20.7',
+    version='0.1.21',
     description='Fetches and parses data from Quora.',
     long_description=long_description,
     author='Christopher Su',
