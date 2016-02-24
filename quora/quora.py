dc990ff02630a1f3690f2721ec0a78dda5062531
Christopher Su <christophersu9@gmail.com>
2016-02-23 17:10:12 -0800

    indentationgis

--- quora/quora.py
+++ quora/quora.py
@@ -1,5 +1,3 @@
-#coding=utf-8
-
 from bs4 import BeautifulSoup
 import re
 import requests
@@ -8,201 +6,201 @@ import requests
 # Helpers
 ####################################################################
 def try_cast_int(s):
-    """ (str) -> int
-    Look for digits in the given string and convert them to the required number.
-    ('2 upvotes') -> 2
-    ('2.2k upvotes') -> 2200
-    """
-    try:
-        pattern = re.compile(r'([0-9]+(\.[0-9]+)*[ ]*[Kk])|([0-9]+)')
-        raw_result = re.search(pattern, s).groups()
-        if raw_result[2] != None:
-            return int(raw_result[2])
-        elif raw_result[1] == None:
-            raw_result = re.search(r'([0-9]+)', raw_result[0])
-            return int(raw_result.groups()[0]) * 1000
-        else:
-            raw_result = re.search(r'([0-9]+)\.([0-9]+)', raw_result[0]).groups()
-            return int(raw_result[0]) * 1000 + int(raw_result[1]) * 100
-    except:
-        return s
+  """ (str) -> int
+  Look for digits in the given string and convert them to the required number.
+  ('2 upvotes') -> 2
+  ('2.2k upvotes') -> 2200
+  """
+  try:
+    pattern = re.compile(r'([0-9]+(\.[0-9]+)*[ ]*[Kk])|([0-9]+)')
+    raw_result = re.search(pattern, s).groups()
+    if raw_result[2] != None:
+      return int(raw_result[2])
+    elif raw_result[1] == None:
+      raw_result = re.search(r'([0-9]+)', raw_result[0])
+      return int(raw_result.groups()[0]) * 1000
+    else:
+      raw_result = re.search(r'([0-9]+)\.([0-9]+)', raw_result[0]).groups()
+      return int(raw_result[0]) * 1000 + int(raw_result[1]) * 100
+  except:
+    return s
 
 def get_question_link(soup):
-    """ (soup) -> str
-    Returns the link at which the question can is present.
-    """
-    question_link = soup.find('a', attrs = {'class' : 'question_link'})
-    return 'http://www.quora.com' + question_link.get('href')
+  """ (soup) -> str
+  Returns the link at which the question can is present.
+  """
+  question_link = soup.find('a', attrs = {'class' : 'question_link'})
+  return 'http://www.quora.com' + question_link.get('href')
 
 def get_author(soup):
-    """ (soup) -> str
-    Returns the name of the author
-    """
-    raw_author = soup.find('div', attrs = {'class' : 'author_info'}).next.get('href')
-    author = raw_author.split('/')[-1]
-    return author
+  """ (soup) -> str
+  Returns the name of the author
+  """
+  raw_author = soup.find('div', attrs = {'class' : 'author_info'}).next.get('href')
+  author = raw_author.split('/')[-1]
+  return author
 
 def extract_username(username):
-    """ (soup) -> str
-    Returns the username of the author
-    """
-    if 'https://www.quora.com/' not in username['href']:
-        return username['href'][1:]
+  """ (soup) -> str
+  Returns the username of the author
+  """
+  if 'https://www.quora.com/' not in username['href']:
+    return username['href'][1:]
+  else:
+    username = re.search("[a-zA-Z-\-]*\-+[a-zA-Z]*-?[0-9]*$", username['href'])
+    if username is not None:
+      return username.group(0)
     else:
-        username = re.search("[a-zA-Z-\-]*\-+[a-zA-Z]*-?[0-9]*$", username['href'])
-        if username is not None:
-            return username.group(0)
-        else:
-            return None
+      return None
 
 ####################################################################
 # API
 ####################################################################
 class Quora:
+  """
+  The class that contains functions required to fetch details of questions and answers.
+  """
+  @staticmethod
+  def get_one_answer(question, author=None):
+    """ (str [, str]) -> dict
+    Fetches one answer and it's details.
     """
-    The class that contains functions required to fetch details of questions and answers.
+    if author is None: # For short URL's
+      if re.match('http', question): # question like http://qr.ae/znrZ3
+        soup = BeautifulSoup(requests.get(question).text)
+      else: # question like znrZ3
+        soup = BeautifulSoup(requests.get('http://qr.ae/' + question).text)
+    else:
+      soup = BeautifulSoup(requests.get('http://www.quora.com/' + question + '/answer/' + author).text)
+    return Quora.scrape_one_answer(soup)
+
+  @staticmethod
+  def scrape_one_answer(soup):
+    """ (soup) -> dict
+    Scrapes the soup object to get details of an answer.
     """
-    @staticmethod
-    def get_one_answer(question, author=None):
-        """ (str [, str]) -> dict
-        Fetches one answer and it's details.
-        """
-        if author is None: # For short URL's
-            if re.match('http', question): # question like http://qr.ae/znrZ3
-                soup = BeautifulSoup(requests.get(question).text)
-            else: # question like znrZ3
-                soup = BeautifulSoup(requests.get('http://qr.ae/' + question).text)
-        else:
-            soup = BeautifulSoup(requests.get('http://www.quora.com/' + question + '/answer/' + author).text)
-        return Quora.scrape_one_answer(soup)
+    try:
+      answer = soup.find('div', id = re.compile('_answer_content$')).find('div', id = re.compile('_container'))
+      question_link = get_question_link(soup)
+      author = get_author(soup)
+      views = soup.find('span', attrs = {'class' : 'stats_row'}).next.next.next.next
+      want_answers = soup.find('span', attrs = {'class' : 'count'}).string
 
-    @staticmethod
-    def scrape_one_answer(soup):
-        """ (soup) -> dict
-        Scrapes the soup object to get details of an answer.
-        """
-        try:
-            answer = soup.find('div', id = re.compile('_answer_content$')).find('div', id = re.compile('_container'))
-            question_link = get_question_link(soup)
-            author = get_author(soup)
-            views = soup.find('span', attrs = {'class' : 'stats_row'}).next.next.next.next
-            want_answers = soup.find('span', attrs = {'class' : 'count'}).string
+      try:
+        upvote_count = soup.find('a', attrs = {'class' : 'vote_item_link'}).find('span', attrs = {'class' : 'count'}).string
+        if upvote_count is None:
+          upvote_count = 0
+      except:
+        upvote_count = 0
 
-            try:
-                upvote_count = soup.find('a', attrs = {'class' : 'vote_item_link'}).find('span', attrs = {'class' : 'count'}).string
-                if upvote_count is None:
-                    upvote_count = 0
-            except:
-                upvote_count = 0
+      try:
+        comment_count = soup.find_all('a', id = re.compile('_view_comment_link'))[-1].find('span').string
+        # '+' is dropped from the number of comments.
+        # Only the comments directly on the answer are considered. Comments on comments are ignored.
+      except:
+        comment_count = 0
 
-            try:
-                comment_count = soup.find_all('a', id = re.compile('_view_comment_link'))[-1].find('span').string
-                # '+' is dropped from the number of comments.
-                # Only the comments directly on the answer are considered. Comments on comments are ignored.
-            except:
-                comment_count = 0
+      answer_stats = map(try_cast_int, [views, want_answers, upvote_count, comment_count])
 
-            answer_stats = map(try_cast_int, [views, want_answers, upvote_count, comment_count])
+      answer_dict = {'views' : answer_stats[0],
+               'want_answers' : answer_stats[1],
+               'upvote_count' : answer_stats[2],
+               'comment_count' : answer_stats[3],
+               'answer' : str(answer),
+               'question_link' : question_link,
+               'author' : author
+              }
+      return answer_dict
+    except:
+      return {}
 
-            answer_dict = {'views' : answer_stats[0],
-                           'want_answers' : answer_stats[1],
-                           'upvote_count' : answer_stats[2],
-                           'comment_count' : answer_stats[3],
-                           'answer' : str(answer),
-                           'question_link' : question_link,
-                           'author' : author
-                          }
-            return answer_dict
-        except:
-            return {}
+  @staticmethod
+  def get_latest_answers(question):
+    """ (str) -> list
+    Takes the title of one question and returns the latest answers to that question.
+    """
+    soup = BeautifulSoup(requests.get('http://www.quora.com/' + question + '/log').text)
+    authors =  Quora.scrape_latest_answers(soup)
+    return [Quora.get_one_answer(question, author) for author in authors]
 
-    @staticmethod
-    def get_latest_answers(question):
-        """ (str) -> list
-        Takes the title of one question and returns the latest answers to that question.
-        """
-        soup = BeautifulSoup(requests.get('http://www.quora.com/' + question + '/log').text)
-        authors =  Quora.scrape_latest_answers(soup)
-        return [Quora.get_one_answer(question, author) for author in authors]
+  @staticmethod
+  def scrape_latest_answers(soup):
+    """ (soup) -> list
+    Returns a list with usernames of those who have recently answered the question.
+    """
+    try:
+      authors = []
+      clean_logs = []
+      raw_logs = soup.find_all('div', attrs={'class' : 'feed_item_activity'})
 
-    @staticmethod
-    def scrape_latest_answers(soup):
-        """ (soup) -> list
-        Returns a list with usernames of those who have recently answered the question.
-        """
-        try:
-            authors = []
-            clean_logs = []
-            raw_logs = soup.find_all('div', attrs={'class' : 'feed_item_activity'})
+      for entry in raw_logs:
+        if 'Answer added by' in entry.next:
+          username = entry.find('a', attrs={'class' : 'user'})
+          if username is not None:
+            username = extract_username(username)
+            if username not in authors:
+              authors.append(username)
+      return authors
+    except:
+      return []
 
-            for entry in raw_logs:
-                if 'Answer added by' in entry.next:
-                    username = entry.find('a', attrs={'class' : 'user'})
-                    if username is not None:
-                        username = extract_username(username)
-                        if username not in authors:
-                            authors.append(username)
-            return authors
-        except:
-            return []
+  @staticmethod
+  def get_question_stats(question):
+    """ (soup) -> dict
+    Returns details about the question.
+    """
+    soup = BeautifulSoup(requests.get('http://www.quora.com/' + question).text)
+    return Quora.scrape_question_stats(soup)
 
-    @staticmethod
-    def get_question_stats(question):
-        """ (soup) -> dict
-        Returns details about the question.
-        """
-        soup = BeautifulSoup(requests.get('http://www.quora.com/' + question).text)
-        return Quora.scrape_question_stats(soup)
+  @staticmethod
+  def scrape_question_stats(soup):
+    """ (soup) -> dict
+    Scrapes the soup object to get details of a question.
+    """
+    try:
+      raw_topics = soup.find_all('span', attrs={'itemprop' : 'title'})
+      topics = []
+      for topic in raw_topics:
+        topics.append(topic.string)
 
-    @staticmethod
-    def scrape_question_stats(soup):
-        """ (soup) -> dict
-        Scrapes the soup object to get details of a question.
-        """
-        try:
-            raw_topics = soup.find_all('span', attrs={'itemprop' : 'title'})
-            topics = []
-            for topic in raw_topics:
-                topics.append(topic.string)
+      want_answers = soup.find('span', attrs={'class' : 'count'}).string
+      answer_count = soup.find('div', attrs={'class' : 'answer_count'}).next.split()[0]
+      question_text = list(soup.find('div', attrs = {'class' : 'question_text_edit'}).find('h1').children)[-1]
+      question_details = soup.find('div', attrs = {'class' : 'question_details_text'})
+      answer_wiki = soup.find('div', attrs = {'class' : 'AnswerWikiArea'}).find('div')
 
-            want_answers = soup.find('span', attrs={'class' : 'count'}).string
-            answer_count = soup.find('div', attrs={'class' : 'answer_count'}).next.split()[0]
-            question_text = list(soup.find('div', attrs = {'class' : 'question_text_edit'}).find('h1').children)[-1]
-            question_details = soup.find('div', attrs = {'class' : 'question_details_text'})
-            answer_wiki = soup.find('div', attrs = {'class' : 'AnswerWikiArea'}).find('div')
+      question_dict = {'want_answers' : try_cast_int(want_answers),
+               'answer_count' : try_cast_int(answer_count),
+               'question_text' : question_text.string,
+               'topics' : topics,
+               'question_details' : str(question_details),
+               'answer_wiki' : str(answer_wiki),
+              }
+      return question_dict
+    except:
+      return {}
 
-            question_dict = {'want_answers' : try_cast_int(want_answers),
-                             'answer_count' : try_cast_int(answer_count),
-                             'question_text' : question_text.string,
-                             'topics' : topics,
-                             'question_details' : str(question_details),
-                             'answer_wiki' : str(answer_wiki),
-                            }
-            return question_dict
-        except:
-            return {}
+  ### Legacy API
+  @staticmethod
+  def get_user_stats(u):
+    """ (str) -> dict
+    Depreciated. Use the User class.
+    """
+    from user import User
+    return User.get_user_stats(u)
 
-    ### Legacy API
-    @staticmethod
-    def get_user_stats(u):
-        """ (str) -> dict
-        Depreciated. Use the User class.
-        """
-        from user import User
-        return User.get_user_stats(u)
+  @staticmethod
+  def get_user_activity(u):
+    """ (str) -> dict
+    Depreciated. Use the User class.
+    """
+    from user import User
+    return User.get_user_activity(u)
 
-    @staticmethod
-    def get_user_activity(u):
-        """ (str) -> dict
-        Depreciated. Use the User class.
-        """
-        from user import User
-        return User.get_user_activity(u)
-
-    @staticmethod
-    def get_activity(u):
-        """ (str) -> dict
-        Depreciated. Use the User class.
-        """
-        from user import User
-        return User.get_activity(u)
+  @staticmethod
+  def get_activity(u):
+    """ (str) -> dict
+    Depreciated. Use the User class.
+    """
+    from user import User
+    return User.get_activity(u)
--- quora/user.py
+++ quora/user.py
@@ -1,5 +1,3 @@
-#coding=utf-8
-
 from bs4 import BeautifulSoup
 from quora import try_cast_int
 import feedparser
@@ -12,162 +10,162 @@ POSSIBLE_FEED_KEYS = ['link', 'id', 'published', 'title', 'summary']
 
 ### Enumerated Types ###
 def enum(*sequential, **named):
-    enums = dict(zip(sequential, range(len(sequential))), **named)
-    reverse = dict((value, key) for key, value in enums.iteritems())
-    enums['reverse_mapping'] = reverse
-    return type('Enum', (), enums)
+  enums = dict(zip(sequential, range(len(sequential))), **named)
+  reverse = dict((value, key) for key, value in enums.iteritems())
+  enums['reverse_mapping'] = reverse
+  return type('Enum', (), enums)
 
 ACTIVITY_ITEM_TYPES = enum(UPVOTE=1, USER_FOLLOW=2, WANT_ANSWER=3, ANSWER=4, REVIEW_REQUEST=5)
-LOG_ENTRY_TYPES     = enum(ANSWER_ADDED=1, ANSWER_DELETED=2, COMMENT=2, EDIT=3, TOPIC=4)
+LOG_ENTRY_TYPES   = enum(ANSWER_ADDED=1, ANSWER_DELETED=2, COMMENT=2, EDIT=3, TOPIC=4)
 
 ####################################################################
 # Helpers
 ####################################################################
 
 def get_name(source):
-    return str(source.find('span', attrs={'class' : 'user'}).string)
+  return str(source.find('span', attrs={'class' : 'user'}).string)
 
 def build_feed_item(item):
-    result = {}
-    keys = POSSIBLE_FEED_KEYS
-    for key in keys:
-        if key in item.keys():
-            result[key] = item[key]
-    return result
+  result = {}
+  keys = POSSIBLE_FEED_KEYS
+  for key in keys:
+    if key in item.keys():
+      result[key] = item[key]
+  return result
 
 def is_want_answer(description):
-    tag  = description.find('span', id = re.compile('^[a-z]*_+[a-z]*_+[0-9]*$'))
-    if tag is not None:
-        return True
-    else:
-        return False
+  tag  = description.find('span', id = re.compile('^[a-z]*_+[a-z]*_+[0-9]*$'))
+  if tag is not None:
+    return True
+  else:
+    return False
 
 def is_author(link, baseurl):
-    author = re.search('[a-zA-Z-\-]*\-+[a-zA-Z]*-?[0-9]*$', link)
-    user   = re.search('com*\/([a-zA-Z]*\-+[a-zA-Z]*-?[a-z-A-Z-0-9]*)\/rss$', baseurl)
-    if user is not None and author is not None:
-        author = author.group(0)
-        user   = user.group(1)
-        return author == user
-    else:
-        return False
+  author = re.search('[a-zA-Z-\-]*\-+[a-zA-Z]*-?[0-9]*$', link)
+  user   = re.search('com*\/([a-zA-Z]*\-+[a-zA-Z]*-?[a-z-A-Z-0-9]*)\/rss$', baseurl)
+  if user is not None and author is not None:
+    author = author.group(0)
+    user   = user.group(1)
+    return author == user
+  else:
+    return False
 
 def is_review(link):
-    if link is not None:
-        match = re.search('^https?:\/\/www\.?quora.com\/Reviews-of[a-zA-Z0-9-\-]*$', link)
-        if match is not None:
-            return True
-        else:
-            return False
+  if link is not None:
+    match = re.search('^https?:\/\/www\.?quora.com\/Reviews-of[a-zA-Z0-9-\-]*$', link)
+    if match is not None:
+      return True
     else:
-        return False
+      return False
+  else:
+    return False
 
 def check_activity_type(entry):
-    description = BeautifulSoup(entry['description'])
-    link        = entry['link']
-    base_url    = entry['summary_detail']['base']
+  description = BeautifulSoup(entry['description'])
+  link    = entry['link']
+  base_url  = entry['summary_detail']['base']
 
-    if entry['description'] == '':
-        return ACTIVITY_ITEM_TYPES.USER_FOLLOW
-    elif is_review(link) is True:
-        return ACTIVITY_ITEM_TYPES.REVIEW_REQUEST
-    elif is_want_answer(description) is True:
-        return ACTIVITY_ITEM_TYPES.WANT_ANSWER
-    elif is_author(link, base_url) is True:
-        return ACTIVITY_ITEM_TYPES.ANSWER
-    else:
-        return ACTIVITY_ITEM_TYPES.UPVOTE
+  if entry['description'] == '':
+    return ACTIVITY_ITEM_TYPES.USER_FOLLOW
+  elif is_review(link) is True:
+    return ACTIVITY_ITEM_TYPES.REVIEW_REQUEST
+  elif is_want_answer(description) is True:
+    return ACTIVITY_ITEM_TYPES.WANT_ANSWER
+  elif is_author(link, base_url) is True:
+    return ACTIVITY_ITEM_TYPES.ANSWER
+  else:
+    return ACTIVITY_ITEM_TYPES.UPVOTE
 
 
 ####################################################################
 # API
 ####################################################################
 class User:
-    def __init__(self, user):
-        self.user = user
-        self._stats = None
-        self._activity = None
+  def __init__(self, user):
+    self.user = user
+    self._stats = None
+    self._activity = None
 
-    @property
-    def stats(self):
-        if self._stats is None:
-            self._stats = self.get_user_stats(self.user)
-        return self._stats
+  @property
+  def stats(self):
+    if self._stats is None:
+      self._stats = self.get_user_stats(self.user)
+    return self._stats
 
-    @property
-    def activity(self):
-        if self._activity is None:
-            self._activity = self.get_user_activity(self.user)
-        return self._activity
+  @property
+  def activity(self):
+    if self._activity is None:
+      self._activity = self.get_user_activity(self.user)
+    return self._activity
 
-    @staticmethod
-    def get_user_stats(user):
-        try:
-            soup = BeautifulSoup(requests.get('http://www.quora.com/' + user).text)
-            data_stats = []
-            name = get_name(soup)
-            err = None
+  @staticmethod
+  def get_user_stats(user):
+    try:
+      soup = BeautifulSoup(requests.get('http://www.quora.com/' + user).text)
+      data_stats = []
+      name = get_name(soup)
+      err = None
 
-            for item in soup.find_all('span', attrs={'class' : 'list_count'}):
-                data_stats.append(item.string)
-            data_stats = map(try_cast_int, data_stats)
+      for item in soup.find_all('span', attrs={'class' : 'list_count'}):
+        data_stats.append(item.string)
+      data_stats = map(try_cast_int, data_stats)
 
-            user_dict = {'answers'   : data_stats[1],
-                         'blogs'     : err,
-                         'edits'     : data_stats[5],
-                         'followers' : data_stats[3],
-                         'following' : data_stats[4],
-                         'name'      : name,
-                         'posts'     : data_stats[2],
-                         'questions' : data_stats[0],
-                         'topics'    : err,
-                         'username'  : user }
-            return user_dict
-        except:
-            return {}
+      user_dict = {'answers'   : data_stats[1],
+             'blogs'   : err,
+             'edits'   : data_stats[5],
+             'followers' : data_stats[3],
+             'following' : data_stats[4],
+             'name'    : name,
+             'posts'   : data_stats[2],
+             'questions' : data_stats[0],
+             'topics'  : err,
+             'username'  : user }
+      return user_dict
+    except:
+      return {}
 
-    @staticmethod
-    def get_user_activity(user):
-        try:
-            f = feedparser.parse('http://www.quora.com/' + user + '/rss')
-            result = {
-                'username': user,
-                'last_updated': f.feed.updated
-            }
-            for entry in f.entries:
-                if 'activity' not in result.keys():
-                    result['activity'] = []
-                result['activity'].append(build_feed_item(entry))
-            return result
-        except:
-            return {}
+  @staticmethod
+  def get_user_activity(user):
+    try:
+      f = feedparser.parse('http://www.quora.com/' + user + '/rss')
+      result = {
+        'username': user,
+        'last_updated': f.feed.updated
+      }
+      for entry in f.entries:
+        if 'activity' not in result.keys():
+          result['activity'] = []
+        result['activity'].append(build_feed_item(entry))
+      return result
+    except:
+      return {}
 
-    @staticmethod
-    def get_activity(user):
-        try:
-            f = feedparser.parse('http://www.quora.com/' + user + '/rss')
-            activity = Activity()
-            for entry in f.entries:
-                activity_type = check_activity_type(entry)
-                if activity_type is not None:
-                    if activity_type == ACTIVITY_ITEM_TYPES.UPVOTE:
-                        activity.upvotes.append(build_feed_item(entry))
-                    elif activity_type == ACTIVITY_ITEM_TYPES.USER_FOLLOW:
-                        activity.user_follows.append(build_feed_item(entry))
-                    elif activity_type == ACTIVITY_ITEM_TYPES.WANT_ANSWER:
-                        activity.want_answers.append(build_feed_item(entry))
-                    elif activity_type == ACTIVITY_ITEM_TYPES.ANSWER:
-                        activity.answers.append(build_feed_item(entry))
-                    elif activity_type == ACTIVITY_ITEM_TYPES.REVIEW_REQUEST:
-                        activity.review_requests.append(build_feed_item(entry))
-            return activity
-        except:
-            return Activity()
+  @staticmethod
+  def get_activity(user):
+    try:
+      f = feedparser.parse('http://www.quora.com/' + user + '/rss')
+      activity = Activity()
+      for entry in f.entries:
+        activity_type = check_activity_type(entry)
+        if activity_type is not None:
+          if activity_type == ACTIVITY_ITEM_TYPES.UPVOTE:
+            activity.upvotes.append(build_feed_item(entry))
+          elif activity_type == ACTIVITY_ITEM_TYPES.USER_FOLLOW:
+            activity.user_follows.append(build_feed_item(entry))
+          elif activity_type == ACTIVITY_ITEM_TYPES.WANT_ANSWER:
+            activity.want_answers.append(build_feed_item(entry))
+          elif activity_type == ACTIVITY_ITEM_TYPES.ANSWER:
+            activity.answers.append(build_feed_item(entry))
+          elif activity_type == ACTIVITY_ITEM_TYPES.REVIEW_REQUEST:
+            activity.review_requests.append(build_feed_item(entry))
+      return activity
+    except:
+      return Activity()
 
 class Activity:
-    def __init__(self, args=None):
-        self.upvotes = []
-        self.user_follows = []
-        self.want_answers = []
-        self.answers = []
-        self.review_requests = []
+  def __init__(self, args=None):
+    self.upvotes = []
+    self.user_follows = []
+    self.want_answers = []
+    self.answers = []
+    self.review_requests = []
