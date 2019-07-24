5a203da51330ba759c8d58d6c2e3c02c740a4cc1
Aldy syahdeini <syahda01@heiway.net>
2019-07-24 16:54:37 +0200

    change iteritems (deprecated) to items. And load module locally

--- quora/__init__.py
+++ quora/__init__.py
@@ -1,9 +1,9 @@
 """
 Package to fetch information from Quora.
-Supports Python 2.7.
+Supports Python 3.7
 Source: https://github.com/csu/pyquora.
 Use help(quora.quora) or help(quora.user) for more help.
 """
 
-from user import User, Activity
-from quora import Quora, try_cast_int
+from .user import User, Activity
+from .quora import Quora, try_cast_int
